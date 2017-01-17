7d14b91d4dcf134742f7de065557d6e3ddb2b2c9
tamilmani <tamilmani@smartglossa>
2017-01-17 14:37:05 +0530

    add files

---
+++ homeproject/src/main/java/com/smartglossa/homeproject/HomeConstant.java
@@ -0,0 +1,8 @@
+package com.smartglossa.homeproject;
+
+public class HomeConstant {
+	final static String MYSQL_SERVER = System.getProperty("home.mysql");
+	final static String USERNAME = System.getProperty("home.username");
+	final static String PASSWORD = System.getProperty("home.password");
+	final static String DATABASE = System.getProperty("home.database");
+}
